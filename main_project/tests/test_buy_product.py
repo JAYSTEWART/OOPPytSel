6288d6bce4498c4b44c5969d07d6d244dcf4a2b3
jaystewart <slipknotsevenjaystewart@gmail.com>
2024-02-22 19:41:26 +0100

    Add new page and methods

--- main_project/tests/test_buy_product.py
+++ main_project/tests/test_buy_product.py
@@ -1,10 +1,6 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from main_project.pages.cart_page import Cart_page
 from main_project.pages.login_page import Login_page
 from main_project.pages.main_page import Main_page
