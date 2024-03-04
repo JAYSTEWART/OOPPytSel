ab778b96e0086a687db3539b7710fb099527781a
jaystewart <slipknotsevenjaystewart@gmail.com>
2024-03-04 19:02:03 +0100

    New products

--- pages/main_page.py
+++ pages/main_page.py
@@ -12,7 +12,9 @@ class Main_page(Base):
         self.driver = driver
 
     # Locators
-    select_product_1 = "//button[@id='add-to-cart-sauce-labs-backpack']"
+    product_1 = "//button[@id='add-to-cart-sauce-labs-backpack']"
+    product_2 = "//button[@id='add-to-cart-sauce-labs-bike-light']"
+    product_3 = "//button[@id='add-to-cart-sauce-labs-fleece-jacket']"
     cart = "//div[@id='shopping_cart_container']"
     button_menu = "//button[@id='react-burger-menu-btn']"
     button_about = "//a[@id='about_sidebar_link']"
@@ -21,7 +23,15 @@ class Main_page(Base):
 
     def get_select_product_1(self):
         return WebDriverWait(self.driver, 30).until(
-            EC.element_to_be_clickable((By.XPATH, self.select_product_1)))
+            EC.element_to_be_clickable((By.XPATH, self.product_1)))
+
+    def get_select_product_2(self):
+        return WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, self.product_2)))
+
+    def get_select_product_3(self):
+        return WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, self.product_3)))
 
     def get_cart(self):
         return WebDriverWait(self.driver, 30).until(
@@ -41,6 +51,14 @@ class Main_page(Base):
         self.get_select_product_1().click()
         print("Click select product")
 
+    def click_select_product_2(self):
+        self.get_select_product_2().click()
+        print("Click select product")
+
+    def click_select_product_3(self):
+        self.get_select_product_3().click()
+        print("Click select product")
+
     def click_cart(self):
         self.get_cart().click()
         print("Click cart")
@@ -55,9 +73,19 @@ class Main_page(Base):
 
     # Methods
 
-    def select_product(self):
+    def select_product_1(self):
         self.get_current_url()
         self.click_select_product_1()
+        self.click_cart()
+
+    def select_product_2(self):
+        self.get_current_url()
+        self.click_select_product_2()
+        self.click_cart()
+
+    def select_product_3(self):
+        self.get_current_url()
+        self.click_select_product_3()
         self.click_cart()
 
     def select_menu_about(self):
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest
 from selenium import webdriver
 from pages.cart_page import Cart_page
 from pages.finish_page import Finish_page
@@ -9,7 +10,8 @@ from pages.payment_page import Payment_page
 from pages.user_information_page import User_information_page
 
 
-def test_buy_product():
+@pytest.mark.order(3)
+def test_buy_product_1():
     driver = webdriver.Chrome()
 
     print('Start Test')
@@ -18,17 +20,72 @@ def test_buy_product():
     login.autorizathion()
 
     mp = Main_page(driver)
-    mp.select_product()
+    mp.select_product_1()
 
     cp = Cart_page(driver)
     cp.click_checkout_button()
 
-    uip = User_information_page(driver)
-    uip.input_data()
+    # uip = User_information_page(driver)
+    # uip.input_data()
+    #
+    # pp = Payment_page(driver)
+    # pp.payment()
+    #
+    # fp = Finish_page(driver)
+    # fp.finish()
+    print('Finish test 1')
+    time.sleep(5)
 
-    pp = Payment_page(driver)
-    pp.payment()
 
-    fp = Finish_page(driver)
-    fp.finish()
+@pytest.mark.order(1)
+def test_buy_product_2():
+    driver = webdriver.Chrome()
+
+    print('Start Test 2')
+
+    login = Login_page(driver)
+    login.autorizathion()
+
+    mp = Main_page(driver)
+    mp.select_product_2()
+
+    cp = Cart_page(driver)
+    cp.click_checkout_button()
+
+    # uip = User_information_page(driver)
+    # uip.input_data()
+    #
+    # pp = Payment_page(driver)
+    # pp.payment()
+    #
+    # fp = Finish_page(driver)
+    # fp.finish()
+    print('Finish test 2')
+    time.sleep(5)
+
+
+@pytest.mark.order(2)
+def test_buy_product_3():
+    driver = webdriver.Chrome()
+
+    print('Start Test 3')
+
+    login = Login_page(driver)
+    login.autorizathion()
+
+    mp = Main_page(driver)
+    mp.select_product_3()
+
+    cp = Cart_page(driver)
+    cp.click_checkout_button()
+
+    # uip = User_information_page(driver)
+    # uip.input_data()
+    #
+    # pp = Payment_page(driver)
+    # pp.payment()
+    #
+    # fp = Finish_page(driver)
+    # fp.finish()
+    print('Finish test 3')
     time.sleep(5)
